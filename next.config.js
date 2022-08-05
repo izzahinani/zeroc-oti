b7fa8a204423104e28aa95fefab1e7306e8f2238
Blahblahblaah <blahblahblaah13@gmail.com>
2022-08-05 21:19:12 +0800

    Base Code

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  env: {
+    customKey: "Y87QZFEYBM480ZG03EP57P4J340N",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
---
+++ pages/api/test.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+export default async function handler(req, res) {
+  if (req.method === "GET") {
+    try {
+      const { data } = await axios.get("https://beta3.api.climatiq.io/emission-factors/sources?year=2021", {
+        headers: { Authorization: `Bearer ${process.env.customKey}` },
+      });
+      res.status(200).json({ data });
+    } catch (err) {
+      res.status(500).json({ message: err.message });
+    }
+  } else {
+    // Handle any other HTTP method
+  }
+}
