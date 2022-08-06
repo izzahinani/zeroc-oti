fa5f827bd668781a538257c5a9669b663d894975
izzahinani <izzahinani.career@gmail.com>
2022-08-07 07:07:52 +0800

    completed select and text input for datapoints page

--- pages/api/test.js
+++ pages/api/test.js
@@ -8,7 +8,7 @@ export default async function handler(req, res) {
       });
       res.status(200).json({ data });
     } catch (err) {
-      res.status(500).json({ message: err.message });
+      res.status(500).json({ zmessage: err.message });
     }
   } else {
     // Handle any other HTTP method
